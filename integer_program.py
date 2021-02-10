062feef05f58c9e58d1d8710bc618a25340c2b68
srepec <54257449+srepec@users.noreply.github.com>
2021-02-10 11:15:47 -0700

    Integer program changes (#3)
    * created a new branch for testing
    
    * created integer program
    
    * created integer program
    
    * future section created
    
    * added one more change

---
+++ integer_program.py
@@ -0,0 +1,13 @@
+# This program adds up integers that have been passed as arguments in the command line
+import sys
+try:
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print ('sum =', total)
+except ValueError:
+    print ('Please supply integer arguments')
+
+# adding new features below
+
+# this section is reserved for future dev
+
+# one more change
